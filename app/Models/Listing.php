d4c4696ba06afd2ccc0e8dce4df4ed42c77b6ba2
= <=>
2022-07-22 15:35:17 +0300

    first commit

---
+++ app/Models/Listing.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+
+class Listing {
+    public static function all(){
+        return[
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales enim sit amet auctor condimentum. In pharetra turpis ut libero tincidunt, nec ultrices elit aliquet.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales enim sit amet auctor condimentum. In pharetra turpis ut libero tincidunt, nec ultrices elit aliquet.'
+            ]
+            ];
+    }
+}
